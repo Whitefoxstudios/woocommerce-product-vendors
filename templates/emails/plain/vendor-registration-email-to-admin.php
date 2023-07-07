9dd1bef4979fc517a6777755022aca1b89d0dcc7
Whitefoxstudios <support@whitefoxstudios.net>
2023-07-07 10:55:53 -0400

    initial upload

---
+++ templates/emails/plain/vendor-registration-email-to-admin.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Vendor registration email to admin (plain text).
+ *
+ * @version 2.0.0
+ * @since 2.0.0
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly
+}
+
+echo "= " . $email_heading . " =\n\n";
+
+echo __( 'Hello! A vendor has requested to be registered.', 'woocommerce-product-vendors' ) . "\n\n";
+
+echo __( 'Vendor Information', 'woocommerce-product-vendors' ) . "\n\n";
+
+echo "=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n\n";
+
+echo sprintf( __( 'Email: %s', 'woocommerce-product-vendors' ), $user_email ) . "\n\n";
+echo sprintf( __( 'First Name: %s', 'woocommerce-product-vendors' ), $first_name ) . "\n\n";
+echo sprintf( __( 'Last Name: %s', 'woocommerce-product-vendors' ), $last_name ) . "\n\n";
+echo sprintf( __( 'Vendor Name: %s', 'woocommerce-product-vendors' ), $vendor_name ) . "\n\n";
+echo __( 'Vendor Description:', 'woocommerce-product-vendors' ) . "\n\n";
+echo $vendor_desc . "\n\n";
+echo "\n=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n\n";
+
+/* translators: %s is the pending vendors list url. */
+printf( esc_html__( 'You can approve this vendor at %s.', 'woocommerce-product-vendors' ), esc_url( admin_url( 'users.php?role=wc_product_vendors_pending_vendor' ) ) );
+
+echo apply_filters( 'woocommerce_email_footer_text', get_option( 'woocommerce_email_footer_text' ) );
